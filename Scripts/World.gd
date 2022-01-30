ea2aac1b4b34161d424d8aee212eb5a18d2c5f42
nadiadek <nadiasilva@ug.uchile.cl>
2022-01-30 14:45:51 -0300

    Se puede reiniciar el juego

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -5,11 +5,17 @@ onready var player = $"Characters/Prota"
 onready var constant = $"Constant"
 onready var GameOverText = $"Constant/Game Over"
 
+var trident_enemy = preload("res://Scenes/Personajes/Enemy-Trident.tscn")
+var gun_enemy = preload("res://Scenes/Personajes/Enemy-Gun.tscn")
+
+
 func _physics_process(delta):
 	var pos_player = player.position
 	
 	constant.position = Vector2(pos_player.x , pos_player.y - 276)
-	
+	if Input.is_action_pressed("restart") and player.dead: 
+		print("lo pesca")
+		get_tree().change_scene("res://Scenes/Principal.tscn")
 	
 func _on_Prota_game_over():
 	GameOverText.show()
